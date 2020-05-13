570f353bc5e38b29d6cfc1663767c58c62776b24
Tausif Ansari <mohammed.tausifansari@gmail.com>
2020-05-13 10:33:43 +0530

    Single Page Applications 1

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -10,12 +10,12 @@ function Footer(props) {
                     <h5>Links</h5>
                     <ul className="list-unstyled">
                         <li><Link to='/home'>Home</Link></li>
-                        <li><Link to='/aboutus'>About Us</Link></li>
                         <li><Link to='/menu'>Menu</Link></li>
-                        <li><Link to='/contactus'>Contact Us</Link></li>
+                        <li><Link to='/about'>About</Link></li>
+                        <li><a href="contactus.html">Contact</a></li>
                     </ul>
                 </div>
-                <div className="col-7 col-sm-4">
+                <div className="col-7 col-sm-5">
                     <h5>Our Address</h5>
                     <address>
 		              121, Clear Water Bay Road<br />
@@ -27,10 +27,10 @@ function Footer(props) {
                          confusion@food.net</a>
                     </address>
                 </div>
-                <div className="col-12 col-sm-3 align-self-center">
+                <div className="col-12 col-sm-4 align-self-center">
                     <div className="text-center">
                         <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-google-plus"></i></a>
-                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook"></i></a>
+                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile"><i className="fa fa-facebook"></i></a>
                         <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
                         <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
                         <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
@@ -38,9 +38,9 @@ function Footer(props) {
                     </div>
                 </div>
             </div>
-            <div className="row">             
-                <div className="col-auto justify-content-center">
-                    <p>© Copyright 2018 Ristorante Con Fusion</p>
+            <div className="row justify-content-center">             
+                <div className="col-auto">
+                    <p>©️ Copyright 2018 Ristorante Con Fusion</p>
                 </div>
             </div>
         </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,15 @@
 import React, { Component }from 'react';
 //import { Navbar, NavbarBrand} from 'reactstrap';
-import Home from './HomeComponent'
+import Contact from './ContactComponent';
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponents';
 import DishDetail from './DishdetailComponent'
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
@@ -14,7 +18,9 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
-      selectedDish: null
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -24,6 +30,9 @@ class Main extends Component {
     const HomePage = () => {
       return(
           <Home 
+            dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+            promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+            leader={this.state.leaders.filter((leader) => leader.featured)[0]}
           />
       );
     }
@@ -34,6 +43,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path='/contactus' component={Contact} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
