c2110932f65a77a08b0d55f2677a582e9e93b64a
Tausif Ansari <mohammed.tausifansari@gmail.com>
2020-05-16 20:00:39 +0530

    React Redux

--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ import Main from './components/MainComponent';
 import './App.css';
 //import { DISHES } from './shared/dishes';
 import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {ConfigureStore} from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends Component {
+
+
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
