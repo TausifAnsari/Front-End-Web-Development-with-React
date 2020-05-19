eeb7b8e3e11c4f44bd86fc384cafb67d5dd6952d
Tausif Ansari <mohammed.tausifansari@gmail.com>
2020-05-19 20:53:48 +0530

    Splitting and Combining Reducers

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,4 @@
-import { DISHES } from '../shared/dishes';
+/*import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -13,3 +13,4 @@ export const initialState = {
 export const Reducer = (state = initialState, action) => {
     return state;
 };
+*/
